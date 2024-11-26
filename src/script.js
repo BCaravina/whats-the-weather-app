ab5380672797b6fa9c9ec061ea1930959c1a4675
Biscatrix <168189345+Biscatrix@users.noreply.github.com>
2024-11-26 16:47:25 -0300

    Changed date and time method to use API information

--- src/script.js
+++ src/script.js
@@ -1,7 +1,20 @@
-function setHour() {
-  let weekDay = document.querySelector("#week-day");
-  let currentTime = document.querySelector("#current-time");
-  let now = new Date();
+function refreshWeatherData(response) {
+  let cityName = document.querySelector(".city-name");
+  let temperatureValue = document.querySelector(".temperature-value");
+  let currentConditions = document.querySelector("#current-condition");
+  let currentConditionsData = response.data.condition.description;
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let dateTime = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
+
+  cityName.innerHTML = response.data.city;
+  temperatureValue.innerHTML = Math.round(response.data.temperature.current);
+  currentConditions.innerHTML =
+    currentConditionsData[0].toUpperCase() + currentConditionsData.slice(1);
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed.toFixed(1)}km/h`;
+
   let weekDays = [
     "Sunday",
     "Monday",
@@ -11,9 +24,8 @@ function setHour() {
     "Friday",
     "Saturday",
   ];
-
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
   if (hours < 10) {
     hours = `0${hours}`;
@@ -27,29 +39,13 @@ function setHour() {
     minutes = minutes;
   }
 
-  weekDay.innerHTML = `${weekDays[now.getDay()]}`;
-  currentTime.innerHTML = `${hours}:${minutes}`;
-}
-
-function refreshWeatherData(response) {
-  let cityName = document.querySelector(".city-name");
-  let temperatureValue = document.querySelector(".temperature-value");
-  let currentConditions = document.querySelector("#current-condition");
-  let currentConditionsData = response.data.condition.description;
-  let humidity = document.querySelector("#humidity");
-  let windSpeed = document.querySelector("#wind-speed");
-
-  cityName.innerHTML = response.data.city;
-  temperatureValue.innerHTML = Math.round(response.data.temperature.current);
-  currentConditions.innerHTML =
-    currentConditionsData[0].toUpperCase() + currentConditionsData.slice(1);
-  humidity.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeed.innerHTML = `${response.data.wind.speed.toFixed(1)}km/h`;
+  dateTime.innerHTML = `<strong>${
+    weekDays[date.getDay()]
+  }</strong> ${date.getHours()}:${date.getMinutes()}`;
 }
 
 function searchCity(city) {
   const apiKey = "c49ed490tce5aa3a51c741aaobee84ef";
-  // let unit = "metric"
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeatherData);
 }
@@ -64,5 +60,4 @@ function handleSearchFormSubmit(event) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchFormSubmit);
 
-setHour();
 searchCity("Lisbon");
