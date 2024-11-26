5b40166fde11c1b4d3dfea8390f119b47dac0e9b
Biscatrix <168189345+Biscatrix@users.noreply.github.com>
2024-11-26 15:05:54 -0300

    Added axios, used api for city name and temperature; minor html updates

--- src/script.js
+++ src/script.js
@@ -15,10 +15,10 @@ function setHour() {
   let hours = now.getHours();
   let minutes = now.getMinutes();
 
-  if (hour < 10) {
-    hour = `0${hour}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
   } else {
-    hour = hour;
+    hours = hours;
   }
 
   if (minutes < 10) {
@@ -31,18 +31,40 @@ function setHour() {
   hoursMinutes.innerHTML = `${hours}:${minutes}`;
 }
 
-function updateCity(event) {
-  event.preventDefault();
-
-  let searchInput = document.querySelector(".search-field").value.trim();
-  let formattedInput =
-    searchInput.charAt(0).toUpperCase() + searchInput.slice(1).toLowerCase();
+function refreshWeatherData(response) {
+  console.log(response.data);
+  console.log(response.data.temperature.current);
+  console.log(response.data.condition.description);
+  console.log(response.data.temperature.humidity);
+  console.log(response.data.wind.speed);
 
   let cityName = document.querySelector(".city-name");
-  cityName.innerHTML = formattedInput;
+  let temperatureValue = document.querySelector(".temperature-value");
+  let currentConditions = document.querySelector("#current-condition");
+  // let humidity = document.querySelector();
+  // let windSpeed = document.querySelector();
+
+  cityName.innerHTML = response.data.city;
+  temperatureValue.innerHTML = Math.round(response.data.temperature.current);
+  currentConditions.innerHTML = response.data.condition.description;
+}
+
+function searchCity(city) {
+  const apiKey = "c49ed490tce5aa3a51c741aaobee84ef";
+  // let unit = "metric"
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", updateCity);
+searchForm.addEventListener("submit", handleSearchFormSubmit);
 
 setHour();
+searchCity("Lisbon");
