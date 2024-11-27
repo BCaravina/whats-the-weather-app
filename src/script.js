6d9a9d597577e3e62259238bd1cbbc4ea4620afc
Biscatrix <168189345+Biscatrix@users.noreply.github.com>
2024-11-27 16:33:00 -0300

    Added 7-day forecast, JS API for forecast, and CSS updates

--- src/script.js
+++ src/script.js
@@ -1,3 +1,44 @@
+function refreshForecastData(response) {
+  const forecastArray = document.querySelectorAll("#forecast-list .days");
+
+  const startDate = new Date(response.data.daily[0].time * 1000);
+  const startDayIndex = (startDate.getDay() + 1) % 7;
+
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  forecastArray.forEach((liElement, index) => {
+    if (response.data.daily[index]) {
+      const forecastItem = response.data.daily[index];
+      const currentDayIndex = (startDayIndex + index) % 7;
+      const weekday = weekDays[currentDayIndex];
+      liElement.innerHTML = `
+        <div class="forecast-day">${weekday}</div>
+        <img class="forecast-icon" src="${
+          forecastItem.condition.icon_url
+        }" alt="Weather Icon" />
+        <div class="forecast-temp">
+          ${Math.round(forecastItem.temperature.minimum)}°C / 
+          <span>${Math.round(forecastItem.temperature.maximum)}°C</span>
+        </div>
+        <div class="forecast-humidity">Humidity: <strong>${
+          forecastItem.temperature.humidity
+        }%</strong></div>
+        <div class="forecast-wind-speed">
+          Wind: <strong>${forecastItem.wind.speed.toFixed(1)}km/h</strong>
+        </div>
+      `;
+    }
+  });
+}
+
 function refreshWeatherData(response) {
   let cityName = document.querySelector(".city-name");
   let temperatureValue = document.querySelector(".temperature-value");
@@ -41,13 +82,16 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  return `<strong>${weekDays[date.getDay()]}</strong> ${hours}:${minutes}`;
+  return `<strong>${weekDays[date.getDay()]}</strong>, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   const apiKey = "c49ed490tce5aa3a51c741aaobee84ef";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeatherData);
+
+  let apiForecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiForecastUrl).then(refreshForecastData);
 }
 
 function handleSearchFormSubmit(event) {
